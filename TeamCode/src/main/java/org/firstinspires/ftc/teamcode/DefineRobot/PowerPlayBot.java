bab7455790b57cdc2a2dbd03888c967399fb2226
AnirudhJagannathan <anirjag123@gmail.com>
2022-11-02 19:58:23 -0700

    LM1 1.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefineRobot/PowerPlayBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DefineRobot/PowerPlayBot.java
@@ -276,6 +276,16 @@ public class PowerPlayBot extends MecanumDrive {
         return imu.getAngularOrientation().firstAngle;
     }
 
+    @Override
+    public Double getExternalHeadingVelocity() {
+        // To work around an SDK bug, use -zRotationRate in place of xRotationRate
+        // and -xRotationRate in place of zRotationRate (yRotationRate behaves as
+        // expected). This bug does NOT affect orientation.
+        //
+        // See https://github.com/FIRST-Tech-Challenge/FtcRobotController/issues/251 for details.
+        return (double) -imu.getAngularVelocity().yRotationRate;
+    }
+
 
     @NonNull
     @Override
